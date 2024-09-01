7f982ac657c75ca71ac029148a53836cf4eec1fc
Imabanana80 <imabanana80.hallo@gmail.com>
2024-09-01 14:35:18 +0800

    chore: why does intellij not actually commit the code on init???

--- src/main/java/com/imabanana80/cosmicSMP/listeners/DimensionChangeListener.java
+++ src/main/java/com/imabanana80/cosmicSMP/listeners/DimensionChangeListener.java
@@ -1,4 +1,51 @@
 package com.imabanana80.cosmicSMP.listeners;
 
-public class DimensionChangeListener {
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.attribute.Attribute;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerPortalEvent;
+import org.bukkit.scoreboard.Scoreboard;
+import org.bukkit.scoreboard.Team;
+
+public class DimensionChangeListener implements Listener {
+    @EventHandler
+    public void onPlayerDimensionChange(PlayerPortalEvent event) {
+        Location to = event.getTo();
+        Player p = event.getPlayer();
+        Scoreboard sb = Bukkit.getScoreboardManager().getMainScoreboard();
+        String teamName = null;
+        for (Team team : sb.getTeams()) {
+            if (team.hasEntry(p.getName())) {
+                teamName = team.getName();
+            }
+        }
+        World.Environment dimension = to.getWorld().getEnvironment();
+        switch (teamName) {
+            case "bac_team_red" -> {
+                if (dimension == World.Environment.NETHER) {
+                    p.getAttribute(Attribute.GENERIC_MAX_HEALTH).setBaseValue(24.0D);
+                } else {
+                    p.getAttribute(Attribute.GENERIC_MAX_HEALTH).setBaseValue(20.0D);
+                }
+            }
+            case "bac_team_green" -> {
+                if (dimension == World.Environment.NORMAL) {
+                    p.getAttribute(Attribute.GENERIC_MAX_HEALTH).setBaseValue(24.0D);
+                } else {
+                    p.getAttribute(Attribute.GENERIC_MAX_HEALTH).setBaseValue(20.0D);
+                }
+            }
+            case "bac_team_purple" -> {
+                if (dimension == World.Environment.THE_END) {
+                    p.getAttribute(Attribute.GENERIC_MAX_HEALTH).setBaseValue(24.0D);
+                } else {
+                    p.getAttribute(Attribute.GENERIC_MAX_HEALTH).setBaseValue(20.0D);
+                }
+            }
+        }
+    }
 }
--- src/main/java/com/imabanana80/cosmicSMP/listeners/PlayerJoinListener.java
+++ src/main/java/com/imabanana80/cosmicSMP/listeners/PlayerJoinListener.java
@@ -1,4 +1,54 @@
 package com.imabanana80.cosmicSMP.listeners;
 
-public class PlayerJoinListener {
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.attribute.Attribute;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.scoreboard.Scoreboard;
+import org.bukkit.scoreboard.Team;
+
+public class PlayerJoinListener implements Listener {
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        Player p = event.getPlayer();
+        Location loc = p.getLocation();
+        Scoreboard sb = Bukkit.getScoreboardManager().getMainScoreboard();
+        String teamName = null;
+        for (Team team : sb.getTeams()) {
+            if (team.hasEntry(p.getName())) {
+                teamName = team.getName();
+            }
+        }
+        World.Environment dimension = loc.getWorld().getEnvironment();
+        switch (teamName) {
+            case "bac_team_red" -> {
+                if (dimension == World.Environment.NETHER) {
+                    p.getAttribute(Attribute.GENERIC_MAX_HEALTH).setBaseValue(24.0D);
+                } else {
+                    p.getAttribute(Attribute.GENERIC_MAX_HEALTH).setBaseValue(20.0D);
+                }
+                p.getAttribute(Attribute.GENERIC_MOVEMENT_SPEED).setBaseValue(0.1);
+            }
+            case "bac_team_green" -> {
+                if (dimension == World.Environment.NORMAL) {
+                    p.getAttribute(Attribute.GENERIC_MAX_HEALTH).setBaseValue(24.0D);
+                } else {
+                    p.getAttribute(Attribute.GENERIC_MAX_HEALTH).setBaseValue(20.0D);
+                }
+                p.getAttribute(Attribute.GENERIC_MOVEMENT_SPEED).setBaseValue(0.115);
+            }
+            case "bac_team_purple" -> {
+                if (dimension == World.Environment.THE_END) {
+                    p.getAttribute(Attribute.GENERIC_MAX_HEALTH).setBaseValue(24.0D);
+                } else {
+                    p.getAttribute(Attribute.GENERIC_MAX_HEALTH).setBaseValue(20.0D);
+                }
+                p.getAttribute(Attribute.GENERIC_MOVEMENT_SPEED).setBaseValue(0.1);
+            }
+        }
+    }
 }
