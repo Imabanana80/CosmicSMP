c2f876ab25e9f52a1712a7b7328801efcc3f2517
PASTOR Charles <azertyuiopcharles@gmail.com>
2024-09-28 11:00:06 +0200

    Code Refactor

--- src/main/java/com/imabanana80/cosmicSMP/CosmicSMP.java
+++ src/main/java/com/imabanana80/cosmicSMP/CosmicSMP.java
@@ -4,11 +4,21 @@ import com.imabanana80.cosmicSMP.listeners.DimensionChangeListener;
 import com.imabanana80.cosmicSMP.listeners.PlayerDamageListener;
 import com.imabanana80.cosmicSMP.listeners.PlayerJoinListener;
 import com.imabanana80.cosmicSMP.listeners.PlayerRespawnListener;
+
+import java.util.Map;
+
 import org.bukkit.Bukkit;
+import org.bukkit.World.Environment;
+import org.bukkit.attribute.Attribute;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class CosmicSMP extends JavaPlugin {
 
+	public static CosmicSMP i;
+	
+	public CosmicSMP() {i=this;}
+	
     @Override
     public void onEnable() {
         Bukkit.getServer().getPluginManager().registerEvents(new DimensionChangeListener(), this);
@@ -16,4 +26,23 @@ public final class CosmicSMP extends JavaPlugin {
         Bukkit.getServer().getPluginManager().registerEvents(new PlayerDamageListener(), this);
         Bukkit.getServer().getPluginManager().registerEvents(new PlayerJoinListener(), this);
     }
+    
+	public static void setHealthAndMovementSpeed(Player p, String teamName) {
+        final double BASE_HEALTH = 20;
+        final double ENVIRONMENT_HEALTH_BOOST = 4;
+        Map<String, Environment> environmentMap = Map.ofEntries(
+        		Map.entry("bac_team_red", Environment.NETHER),
+        		Map.entry("bac_team_green", Environment.NORMAL),
+        		Map.entry("bac_team_dark_purple", Environment.THE_END)
+        		);
+        Map<String, Double> movementSpeedMap = Map.ofEntries(
+        		Map.entry("bac_team_red", .1),
+        		Map.entry("bac_team_green", .115),
+        		Map.entry("bac_team_dark_purple", .1)
+        		);
+        
+        p.getAttribute(Attribute.GENERIC_MAX_HEALTH).setBaseValue(p.getWorld().getEnvironment().equals(environmentMap.get(teamName)) ? BASE_HEALTH + ENVIRONMENT_HEALTH_BOOST : BASE_HEALTH);
+        p.getAttribute(Attribute.GENERIC_MOVEMENT_SPEED).setBaseValue(movementSpeedMap.getOrDefault(teamName, .1));
+	}
+    
 }
--- src/main/java/com/imabanana80/cosmicSMP/listeners/PlayerDamageListener.java
+++ src/main/java/com/imabanana80/cosmicSMP/listeners/PlayerDamageListener.java
@@ -1,39 +1,36 @@
 package com.imabanana80.cosmicSMP.listeners;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
 import org.bukkit.scoreboard.Scoreboard;
-import org.bukkit.scoreboard.Team;
 
 public class PlayerDamageListener implements Listener {
-    @EventHandler
-    public void onDamage(EntityDamageEvent event) {
-        if (event.getEntity() instanceof Player) {
-            if (event.getEntity() instanceof Player p) {
-                Scoreboard sb = Bukkit.getScoreboardManager().getMainScoreboard();
-                String teamName = null;
-                for (Team team : sb.getTeams()) {
-                    if (team.hasEntry(p.getName())) {
-                        teamName = team.getName();
-                    }
-                }
-                switch (teamName) {
-                    case "bac_team_dark_purple" -> {
-                        if (event.getCause().equals(EntityDamageEvent.DamageCause.FALL)) {
-                            event.setCancelled(true);
-                        }
-                    }
-                    case "bac_team_red" -> {
-                        if (event.getCause().equals(EntityDamageEvent.DamageCause.LAVA) || event.getCause().equals(EntityDamageEvent.DamageCause.FIRE) || event.getCause().equals(EntityDamageEvent.DamageCause.FIRE_TICK)) {
-                            event.setCancelled(true);
-                        }
-                    }
-                }
-            }
-        }
 
-    }
+	@EventHandler
+	public void onDamage(EntityDamageEvent event) {
+		if (!(event.getEntity() instanceof Player p)) return;
+		Scoreboard sb = Bukkit.getScoreboardManager().getMainScoreboard();
+
+		Map<String, List<DamageCause>> map = Map.ofEntries(
+				Map.entry("bac_team_dark_purple", Arrays.asList(DamageCause.FALL)),
+				Map.entry("bac_team_red", Arrays.asList(DamageCause.LAVA,DamageCause.FIRE,DamageCause.FIRE_TICK))
+				);
+
+		if(sb.getTeams().stream()
+				.filter(t->t.hasEntry(p.getName()))
+				.filter(t->map.getOrDefault(t.getName(), new ArrayList<DamageCause>()).contains(event.getCause()))
+				.findAny()
+				.isPresent()) {
+			event.setCancelled(true);
+		}
+	}
 }
